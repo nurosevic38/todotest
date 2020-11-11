9a228f9c7800e5d57775c0e95edc282e316133d5
nurosevic38 <nurosevic38@gmail.com>
2020-11-11 12:23:11 +0100

    Added dropdown, list, listitem and navbar components. Added listiview view.

--- todo-frontend/src/store/index.js
+++ todo-frontend/src/store/index.js
@@ -2,9 +2,12 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
-
+// U ovom primeru imamo samo baseUrl, koji setujemo kako bismo iz cele aplikacije
+// znali na kojoj adresi nam je backend server
 export default new Vuex.Store({
-  state: {},
+  state: {
+    baseUrl: 'http://localhost/todofullstack/todo-backend'
+  },
   mutations: {},
   actions: {},
   modules: {}
